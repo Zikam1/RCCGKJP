d5773b31b77db9cec7cd286d1ae0d15fb284d96f
Isaac Aku <isaac.aku@svr.cyphercrescent.com>
2024-11-19 12:18:41 +0100

    feat: added mission and vision

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    assetsInclude: ['**/*.PNG'] 
+   } // Add this line to include PNG files as assets
 })
