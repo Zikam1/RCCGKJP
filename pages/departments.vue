d85be42bf11f8fafdaf528b498bfa3b2b0ec5bd5
Zikam1 <isaac.aku@cyphercrescent.com>
2024-12-19 11:52:01 +0100

    fest:added the name

--- pages/departments.vue
+++ pages/departments.vue
@@ -37,7 +37,7 @@
           <button
             class="px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition"
           >
-            Learn More
+            Learn More  -
           </button>
         </div>
       </div>
